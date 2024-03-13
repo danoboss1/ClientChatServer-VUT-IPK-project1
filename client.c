1ce5ee76d4ab9eb284e99dc37cc95a99ad70d66a
danoboss1 <danosehnoutek159@gmail.com>
2024-03-13 11:20:39 +0100

    Feature: Start of argument parsing

---
+++ client.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void process_arguments(int argc, char **argv){
+    if(strcmp(argv[1], "-h") == 0) {
+        printf("Usage:\n");
+        printf("    Help: [-h]\n");
+        printf("    Mandatory parameters: program_name -t <tcp|udp> -s <server_address>\n");
+        printf("    Optional parameters: [-p <server_port>] [-d <udp_timeout>] [-r <max_retransmissions>]\n");
+    }
+}
+
+int main(int argc, char **argv){
+    process_arguments(argc, argv);
+    return 0;
+}
