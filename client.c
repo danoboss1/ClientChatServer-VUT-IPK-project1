82ede42c38951ba0550a89670f9c7af23df5a439
danoboss1 <danosehnoutek159@gmail.com>
2024-03-18 15:00:39 +0100

    Feature: Function for receiving UDP messages, structure for messages

--- client.c
+++ client.c
@@ -3,14 +3,41 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+// vycistit si kod od nepotrebneho bordelu
+// zistit co vsetko ako funguje a okomentovat si to 
+// loop na spravy
+// parameters handle
+// rozdelenie spravy na tie jednotlive veci
+// specifikacie vymienania sprav
+// logika stavoveho automatu
+
 #define PORT 4567
 #define MAXLINE 100
 
+// Define message types
+#define CONFIRM 0x00
+#define REPLY   0x01
+#define AUTH    0x02
+#define JOIN    0x03
+#define MSG     0x04
+#define ERR     0xFE
+#define BYE     0xFF
+
+
+// Define message structure
+struct Message {
+    uint8_t type;
+    uint16_t messageID;
+    char content[1400];
+};
+
+// tuto mozem naimplementovat cele handlovanie parametrov
 void process_arguments(int argc, char *argv[]){
     if(argc == 1){
         return;
@@ -22,6 +49,15 @@ void process_arguments(int argc, char *argv[]){
         printf("    Mandatory parameters: program_name -t <tcp|udp> -s <server_address>\n");
         printf("    Optional parameters: [-p <server_port>] [-d <udp_timeout>] [-r <max_retransmissions>]\n");
     }
+}
+
+// Function to receive message
+struct Message receiveMessage(int sockfd, struct sockaddr_in* server_addr) {
+    struct Message msg;
+    socklen_t len = sizeof(*server_addr);
+    // Receive message
+    recvfrom(sockfd, &msg, sizeof(msg), 0, (struct sockaddr*)server_addr, &len);
+    return msg;
 }
 
 // AF_INET = IPV4
@@ -61,7 +97,7 @@ int main(int argc, char *argv[]){
     servaddr.sin_port = htons(PORT);
     servaddr.sin_family = AF_INET;
 
-    printf("Nikde som nic nevytvoril");
+    puts("Nikde som nic nevytvoril");
 
     // Create datagram socket
     int family = AF_INET;
@@ -72,8 +108,15 @@ int main(int argc, char *argv[]){
         perror("ERROR: socket");
         exit(EXIT_FAILURE);
         }
+    
+    //bindovanie serveru na mna
+    struct sockaddr_in bind_addr;
+    memset(&bind_addr, 0, sizeof(struct sockaddr_in));
+    bind_addr.sin_family = AF_INET;
+    bind_addr.sin_port = htons(PORT + 1);
+    bind(client_socket, (struct sockaddr*)&bind_addr, sizeof(struct sockaddr_in));
 
-    printf("Neposlal som nikde nic");
+    puts("Neposlal som nikde nic");
 
     // Send message to the server
     if (sendto(client_socket, message, strlen(message), 0, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
@@ -82,7 +125,11 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
-    printf("Dostal som sa za sendto");
+    puts("Dostal som sa za sendto");
+
+    // toto je na receiveMessage funkciu
+    struct Message receivedMsg;
+    receivedMsg = receiveMessage(client_socket, &servaddr);
 
     // TU JE CHYBA
     // // Receive response from the server
