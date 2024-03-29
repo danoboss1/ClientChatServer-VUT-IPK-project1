ed4578e8baad89462667069db73ec605b0f6d3f2
danoboss1 <danosehnoutek159@gmail.com>
2024-03-29 14:13:04 +0100

    FEATURE: Handle user input in tcp

--- tcp.c
+++ tcp.c
@@ -17,6 +17,73 @@
 #include "tcp.h"
 #include "message_structure.h"
 
+char display_name[DISPLAY_NAME_MAX_LENGTH + 1] = "";
+
+
+void Handle_user_input(const char *input_line, char *line_to_send_from_client, int sockfd){
+    char *AUTH_COMMAND_TO_CHECK = "/auth";
+    char *JOIN_COMMAND_TO_CHECK = "/join";
+    char *RENAME_COMMAND_TO_CHECK = "/rename";
+    char *HELP_COMMAND_TO_CHECK = "/help";
+
+    // AUTH PREHADZUJEM PRIDE MENO,SECRET,DNAME A POSIELAM MENO DNAME SECRET
+    if (strncmp(input_line, AUTH_COMMAND_TO_CHECK, strlen(AUTH_COMMAND_TO_CHECK)) == 0) {
+        char delete_auth_zaciatok[5] = "";
+        char username[USERNAME_MAX_LENGTH + 1] = "";
+        char secret[SECRET_MAX_LENGHT + 1] = "";
+
+        // tato FUNKCIA MI TO ROZDELI DO PREMENNYCH
+        sscanf(input_line, "%s %s %s %s", delete_auth_zaciatok, username, secret, display_name);
+
+        sprintf(line_to_send_from_client, "AUTH %s AS %s USING %s\r\n", username, display_name, secret);
+
+
+        // AUTH {Username} AS {DisplayName} USING {Secret}\r\n
+    } else if (strncmp(input_line, JOIN_COMMAND_TO_CHECK, strlen(JOIN_COMMAND_TO_CHECK)) == 0) {
+        char delete_join_zaciatok[5] = "";
+        char channelID[CHANNEL_ID_MAX_LENGTH + 1] = "";
+
+        sscanf(input_line, "%s %s", delete_join_zaciatok, channelID);
+
+        sprintf(line_to_send_from_client, "JOIN %s AS %s\r\n", channelID, display_name);
+
+        // JOIN {ChannelID} AS {DisplayName}\r\n
+    } else if (strncmp(input_line, RENAME_COMMAND_TO_CHECK, strlen(RENAME_COMMAND_TO_CHECK)) == 0) {
+        char delete_rename_zaciatok[7] = "";
+
+        sscanf(input_line, "%s %s", delete_rename_zaciatok, display_name);
+
+        // tu menim iba display name a nic neposielam
+    } else if (strncmp(input_line, HELP_COMMAND_TO_CHECK, strlen(HELP_COMMAND_TO_CHECK)) == 0) {
+        printf("Commands:\n");
+        printf("  /auth\n");
+        printf("  PARAMETERS: {Username} {Secret} {DisplayName}\n");
+        printf("  DESCRIPTION: Sends AUTH message with the data provided from the command to the server");
+        printf("  /join\n");
+        printf("  PARAMETERS: {channelID}\n");
+        printf("  DESCRIPTION: Sends JOIN message with channel name from the command to the server");
+        printf("  /rename\n");
+        printf("  PARAMETERS: {DisplayName}\n");
+        printf("  DESCRIPTION: Locally changes the display name of the user to be sent with new messages/selected commands");
+        printf("  /help\n");
+        printf("  PARAMETERS: None\n");
+        printf("  DESCRIPTION: Prints out supported local commands with their parameters and a description");
+    } 
+
+    size_t message_size  = strlen(line_to_send_from_client);
+
+    ssize_t bytes_sent;
+
+    // Use write() function to send data
+    bytes_sent = write(sockfd, line_to_send_from_client, message_size);
+    if (bytes_sent < 0) {
+        perror("ERROR: sending message");
+        exit(EXIT_FAILURE);
+    }
+    // write a read
+    // vlozim do prazdneho bufferu spravu a zistim aky velky je ten buffer
+}
+
 
 void tcp_main(struct sockaddr_in servaddr, int server_port) {
     char input_line[FULL_MESSAGE_BUFFER + 1];
@@ -56,7 +123,7 @@ void tcp_main(struct sockaddr_in servaddr, int server_port) {
         // // Wait for events on multiple file descriptors
         int ret = poll(fds, 2, -1); // -1 for indefinite timeout
 
-        char *AUTH_COMMAND_TO_CHECK = "/auth";
+        // char *AUTH_COMMAND_TO_CHECK = "/auth";
 
         // nieco mi prislo na standartny vstup
         if (fds[0].revents & POLLIN) {
@@ -64,38 +131,18 @@ void tcp_main(struct sockaddr_in servaddr, int server_port) {
 
             if (current_state == START_STATE){
 
-                // display name musim ukladat, ale az ked mi to server prijme
-            
-            // AUTH PREHADZUJEM PRIDE MENO,SECRET,DNAME A POSIELAM MENO DNAME SECRET
-            if (strncmp(input_line, AUTH_COMMAND_TO_CHECK, strlen(AUTH_COMMAND_TO_CHECK)) == 0) {
-                char delete_auth_zaciatok[5] = "";
-                char username[USERNAME_MAX_LENGTH + 1] = "";
-                char display_name[DISPLAY_NAME_MAX_LENGTH + 1] = "";
-                char secret[SECRET_MAX_LENGHT + 1] = "";
-
-                // tato FUNKCIA MI TO ROZDELI DO PREMENNYCH
-                sscanf(input_line, "%s %s %s %s", delete_auth_zaciatok, username, secret, display_name);
-
-                sprintf(line_to_send_from_client, "AUTH %s AS %s USING %s\r\n", username, display_name, secret);
-                size_t message_size  = strlen(line_to_send_from_client);
-
-                ssize_t bytes_sent;
-
-                // Use write() function to send data
-                bytes_sent = write(sockfd, line_to_send_from_client, message_size);
-                if (bytes_sent < 0) {
-                    perror("ERROR: sending message");
-                    exit(EXIT_FAILURE);
-                }
-                // write a read
-                // vlozim do prazdneho bufferu spravu a zistim aky velky je ten buffer
-
                 current_state = AUTH_STATE;
 
                 // AUTH {Username} AS {DisplayName} USING {Secret}\r\n
+
+                // write a read
+                // vlozim do prazdneho bufferu spravu a zistim aky velky je ten buffer
+
+
+                // AUTH {Username} AS {DisplayName} USING {Secret}\r\n
+
             }
         }
-    }
     // /r/n bude v tom bufferi, ktory posielam aj v bufferi ktory prijimam
     
     /*
